61a61f873b2f07e0e8ce940f99d1f6081103e221
Miqayel Avagyan <miqayel.avagyan@outlook.com>
2019-08-09 14:07:31 +0400

    adding initial migrations

--- ProjectBuilder.WebAPI/Startup.cs
+++ ProjectBuilder.WebAPI/Startup.cs
@@ -37,9 +37,8 @@ namespace ProjectBuilder.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDefaultIdentity<User>()
-                    .AddRoles<IdentityRole>()
-                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                      options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddCors(options =>
             {
