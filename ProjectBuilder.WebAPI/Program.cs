c3c18895fadf9dd2d353ab9787e37995c06e5598
Miqayel Avagyan <miqayel.avagyan@outlook.com>
2019-08-16 19:55:01 +0400

    t

--- ProjectBuilder.WebAPI/Program.cs
+++ ProjectBuilder.WebAPI/Program.cs
@@ -9,7 +9,7 @@ namespace ProjectBuilder.WebAPI
         {
             CreateWebHostBuilder(args).Build().Run();
         }
-
+        //
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
